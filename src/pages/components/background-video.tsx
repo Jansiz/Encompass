bd83bda8038ceca0dcb1e7b7720dde7b49ef3994
Jansiz <ali.jansizzz@gmail.com>
2024-07-22 00:11:03 -0400

    refactor

--- src/pages/components/background-video.tsx
+++ src/pages/components/background-video.tsx
@@ -4,7 +4,7 @@ import './background-video.css'
 export default function Video(){
 return (
     <div className='video-background'>
-      <video src={Myvideo} autoPlay loop muted />
+      <video src={Myvideo} autoPlay loop muted playsInline />
       </div>
      );
      }
